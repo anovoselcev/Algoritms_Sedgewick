fbe629af7f76955f61ddbbf9ecc9a39a73a92efe
smileamaxdev <novoseltsev.as@phystech.edu>
2020-08-30 17:14:11 +0300

    quick union

---
+++ 1.Introduction/2/2.cpp
@@ -0,0 +1,22 @@
+﻿// Решение задачи связности методом быстрого объединения
+#include <iostream>
+#include <array>
+#include <numeric>
+
+constexpr int N = 10000;
+
+int main() {
+    int p, q;
+    std::array<int, N> id;
+    std::iota(id.begin(), id.end(), 0);
+    while (std::cin >> p >> q) {
+        int i, j;
+        for (i = 0; i != id[i]; i = id[i]);
+        for (j = q; j != id[j]; j = id[j]);
+        if (i == j)
+            continue;
+        id[i] = j;
+        std::cout << " " << p << " " << q << std::endl;
+    }
+    return 0;
+}
