42067e4ed80ca9aba13113882dfc2cb80bd2726e
smileamaxdev <novoseltsev.as@phystech.edu>
2020-09-07 22:47:51 +0300

    balance fast union

---
+++ 1.Introduction/3/3.cpp
@@ -0,0 +1,29 @@
+﻿// Взвешенная версия быстрого объединения
+#include <iostream>
+#include <array>
+#include <numeric>
+
+constexpr int N = 10000;
+
+int main(){
+	int i, j, p, q;
+	std::array<int, N> id;
+	std::array<int, N> sz;
+	sz.fill(1);
+	std::iota(id.begin(), id.end(), 0);
+	while (std::cin >> p >> q) {
+		for (i = p; i != id[i]; i = id[i]);
+		for (j = q; j != id[j]; j = id[j]);
+		if (i == j) continue;
+		if (sz[i] < sz[j]) {
+			id[i] = j;
+			sz[j] += sz[i];
+		}
+		else {
+			id[j] = i;
+			sz[i] += sz[j];
+		}
+		std::cout << " " << p << " " << q << std::endl;
+	}
+	return 0;
+}
