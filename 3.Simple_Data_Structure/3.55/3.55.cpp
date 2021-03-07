ea154ebff45411df5d4f2b21cb92f1c106384aaa
smileamaxdev <novoseltsev.as@phystech.edu>
2021-03-07 17:17:26 +0300

    count characters in string

---
+++ 3.Simple_Data_Structure/3.55/3.55.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+#include <map>
+#include <vector>
+#include <ranges>
+#include <algorithm>
+
+void FreqTable(const char* str) {
+    std::map<char, size_t> freq;
+    for (const char* ch = str; *ch != '\0'; ch++)
+        freq[*ch]++;
+    std::vector<std::pair<size_t, char>> tmp;
+    tmp.reserve(freq.size());
+    for (const auto& el : freq)
+        tmp.push_back({ el.second, el.first });
+    std::ranges::sort(tmp);
+    for(const auto& el : tmp | std::views::reverse)
+        std::cout << el.second << ": " << el.first << std::endl;
+}
+
+int main(){
+    FreqTable("sgredgaefcac weFWEGRGFAWEFAWGDCVDFG QWDQ QWEDADefsfdf");
+}
