7a43f04c69eea6d83d1ca8f0ccc658c8f6549ebd
smileamaxdev <novoseltsev.as@phystech.edu>
2021-03-07 17:47:12 +0300

    is palindrom

---
+++ 3.Simple_Data_Structure/3.56/3.56.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+bool isPalindrom(const char* str, size_t len = 0) {
+	if (!len)
+		for (const char* ch = str; *ch != '\0'; ++ch) len++;
+	for (size_t i = 0; i < len / 2; ++i)
+		if (str[i] != str[len - 1 - i]) return false;
+	return true;
+}
+
+int main() {
+	std::cout << isPalindrom("uyu") << std::endl;
+	std::cout << isPalindrom("uyu", 3) << std::endl;
+	std::cout << isPalindrom("rtre", 4) << std::endl;
+	std::cout << isPalindrom("rtre") << std::endl;
+	return 0;
+}
