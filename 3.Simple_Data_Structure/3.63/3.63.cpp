5c76aed717c5e0e2a97f140a9d6f39ff9c1391f6
smileamaxdev <novoseltsev.as@phystech.edu>
2021-03-08 22:33:52 +0300

    sort dynamic strings

---
+++ 3.Simple_Data_Structure/3.63/3.63.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+
+int compare(const void* lhs,
+            const void* rhs) {
+    return strcmp(*(char**)(lhs), *(char**)(rhs));
+}
+
+
+
+int main(){
+
+    const size_t N = 10000;
+    const size_t buf = 100;
+    char* data[N];
+    for (size_t i = 0; i < N; ++i) {
+        char* tmp = new char[100];
+        if (!std::cin >> *tmp) break;
+        data[i] = tmp;
+    }
+    qsort(data, N, sizeof(char*), compare);
+    std::cout << "Hello World!\n";
+}
