91662e5f9fc0d7f5720c6f78cc0aa3b36666f562
smileamaxdev <novoseltsev.as@phystech.edu>
2021-03-27 23:08:38 +0300

    calculate lg(N!) with recursion

---
+++ 5.Trees_and_Recursion/5.1/5.1.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+#include <cmath>
+
+double LogOFfactorial(size_t N) {
+    if (N == 0) return 0;
+    return std::log10(N) + LogOFfactorial(N - 1);
+}
+
+int main(){
+    size_t N = 0;
+    std::cin >> N;
+    std::cout << LogOFfactorial(N);
+}
