f1a9af76983ced7c0c5a7ad8d68965a9bb47403e
smileamaxdev <novoseltsev.as@phystech.edu>
2021-03-27 22:24:41 +0300

    adjacency matrix based on linked list

---
+++ 3.Simple_Data_Structure/3.72/3.72.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+
+struct Node {
+    size_t v;
+    Node* next = nullptr;
+};
+
+using link = Node*;
+
+int main(int argc, char* argv[]){
+    size_t V = 0;
+    std::cin >> V;
+    link* adj = new link[V](nullptr);
+    size_t a = 0, b = 0;
+    while (std::cin >> a >> b) {
+        adj[a] = new Node{ b, adj[a] };
+        adj[b] = new Node{ a, adj[b] };
+    }
+}
