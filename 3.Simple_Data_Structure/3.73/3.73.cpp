103ba3b9849440e6417962a60398d5ea56971b5f
smileamaxdev <novoseltsev.as@phystech.edu>
2021-03-27 22:42:19 +0300

    count how much vertexes c satisfies a->c and c->b

---
+++ 3.Simple_Data_Structure/3.73/3.73.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+
+int main(){
+    size_t V;
+    std::cin >> V;
+    bool** adj = new bool* [V];
+    for (size_t i = 0; i < V; ++i) {
+        adj[i] = new bool[V](false);
+        adj[i][i] = true;
+    }
+    size_t a, b, count = 0;
+    while (std::cin >> a >> b) {
+        for (size_t i = 0; i < V; ++i)
+            if (adj[a][i] && adj[i][b]) count++;
+    }
+}
