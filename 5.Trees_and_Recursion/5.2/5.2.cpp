490b7fbad3f8dd0daccfaf4fa63402886ef2f5e2
smileamaxdev <novoseltsev.as@phystech.edu>
2021-03-27 23:29:30 +0300

    calculate N! mod M with recursion

---
+++ 5.Trees_and_Recursion/5.2/5.2.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+
+size_t Fact(size_t N) {
+    if (N == 0) return 1;
+    return N * Fact(N - 1);
+}
+
+size_t FindMod(size_t N, size_t M) {
+    if (N >= M) return 0;
+    return Fact(N);
+}
+
+int main(){
+    size_t N, M;
+    std::cin >> N >> M;
+    std::cout << FindMod(N, M);
+}
