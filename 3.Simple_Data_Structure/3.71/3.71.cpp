a73993e3448555a53b4c6024e470e9cd26aee0de
smileamaxdev <novoseltsev.as@phystech.edu>
2021-03-27 22:13:04 +0300

    adjacency matrix based on dynamic massive

---
+++ 3.Simple_Data_Structure/3.71/3.71.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+
+int main(int argc, char* argv[]){
+
+    size_t V;
+    std::cin >> V;
+    bool** adj = new bool* [V];
+    for (size_t i = 0; i < V; ++i) {
+        adj[i] = new bool[V];
+        std::memset(&adj[i], false, V);
+        adj[i][i] = 1;
+    }
+    size_t a = 0, b = 0;
+    while (std::cin >> a >> b) {
+        adj[a][b] = true;
+        adj[b][a] = true;
+    }
+}
+
