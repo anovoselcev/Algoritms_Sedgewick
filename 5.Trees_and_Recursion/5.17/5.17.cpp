e202557e065e9c5b0f67266ae8f3b3b872de6f86
smileamaxdev <novoseltsev.as@phystech.edu>
2021-03-30 17:23:35 +0300

    recursion max for forward list

---
+++ 5.Trees_and_Recursion/5.17/5.17.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+
+template<typename T>
+struct Node{
+    T value;
+    Node* next = nullptr;
+};
+
+template<typename T>
+T max(Node<T>* head) {
+    if (!head) return T{};
+
+    T m = max(head->next);
+    if (head->value > m) return head->value;
+    else return m;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
