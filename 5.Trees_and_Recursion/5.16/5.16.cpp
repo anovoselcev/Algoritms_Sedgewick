70257d96895ebdc5ea9d5ce657471721fb6643ea
smileamaxdev <novoseltsev.as@phystech.edu>
2021-03-30 17:13:20 +0300

    recursion max

---
+++ 5.Trees_and_Recursion/5.16/5.16.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+
+int max(int* data, size_t idx, size_t size) {
+    int m = data[idx];
+    if (idx == size - 1) return m;
+    int n_max = max(data, ++idx, size);
+    if (m > n_max) return m;
+    else return n_max;
+}
+
+int main(){
+    int data[12] = { 0 ,12, 24, 31, -121, 23, 32, 121, 65, 56, 123, 67 };
+    std::cout << std::max(data[0], max(&data[0], 1, 12));
+}
