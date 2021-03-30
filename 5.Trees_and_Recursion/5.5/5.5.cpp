51c477e1896f47db9a50cad753c9940f123de3a6
smileamaxdev <novoseltsev.as@phystech.edu>
2021-03-30 14:42:41 +0300

    plain gcd

---
+++ 5.Trees_and_Recursion/5.5/5.5.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+
+
+
+int main(){
+    int a, b;
+    std::cin >> a >> b;
+    while (a && b) {
+        if (abs(a) > abs(b)) a = a % b;
+        else b = b % a;
+    }
+    std::cout << std::max(a, b) << std::endl;
+}
+
