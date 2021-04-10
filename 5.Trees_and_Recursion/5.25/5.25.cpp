343a2779a149c23300b00b70773a4be20e40d718
smileamaxdev <novoseltsev.as@phystech.edu>
2021-04-11 01:24:54 +0300

    matrix of binary numbers

---
+++ 5.Trees_and_Recursion/5.25/5.25.cpp
@@ -0,0 +1,36 @@
+﻿
+#include <iostream>
+#include <sstream>
+
+void rank(size_t N, bool** numbers, size_t idx) {
+    if (idx < N) {
+        size_t i = std::pow(2, N - 1) - 1;
+        std::memcpy(&numbers[idx][0], &numbers[idx - 1][0], std::pow(2, N - 1));
+        while (numbers[idx][i]) { i--; }
+        numbers[idx][i++] = true;
+        for (; i < std::pow(2, N - 1); i++) numbers[idx][i] = false;
+
+        rank(N, numbers, idx + 1);
+    }
+}
+
+
+int main(){
+    size_t N;
+    std::cin >> N;
+    bool** numbers = new bool* [N];
+    for (size_t i = 0; i < N; ++i) numbers[i] = new bool[std::pow(2, N - 1)](false);
+    rank(N, numbers, 1);
+    std::cout << 0 << std::endl;
+    for (size_t i = 1; i < N; ++i) {
+        std::stringstream ss;
+        bool start = false;
+        for (size_t j = 0; j < std::pow(2, N - 1); j++) {
+            if (numbers[i][j] == true && !start) start = true;
+            if(start) ss << numbers[i][j];
+        }
+        std::cout << ss.str() << std::endl;
+    }
+    return 0;
+}
+
