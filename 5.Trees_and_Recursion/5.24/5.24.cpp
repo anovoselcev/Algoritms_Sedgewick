51e2cbe45ae7b7023dcfb50ad1dabd48ecc07893
smileamaxdev <novoseltsev.as@phystech.edu>
2021-04-10 21:35:39 +0300

    sequence solution for hanoy tower

---
+++ 5.Trees_and_Recursion/5.24/5.24.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <vector>
+
+struct State {
+    size_t count,
+           src,
+           tmp,
+           dst;
+    int step;
+};
+
+void tower(size_t count, size_t src, size_t dst, size_t tmp) {
+    std::vector<State> stack;
+    stack.push_back({ count, src, tmp, dst, 0 });
+    while (!stack.empty()) {
+        auto& state = stack.back();
+        if (state.step == 0) {
+            if (!state.count) stack.pop_back();
+            else {
+                state.step++;
+                stack.push_back({ state.count - 1, state.src, state.dst, state.tmp,0 });
+            }
+        }
+        else if (state.step == 1) {
+            state.step++;
+            stack.push_back({ state.count - 1, state.tmp, state.dst, state.src, 0 });
+        }
+        else stack.pop_back();
+
+    }
+}
+
+
+int main(){
+    size_t N, src, dst, tmp;
+    std::cin >> N >> src >> dst >> tmp;
+    tower(N, src, dst, tmp);
+    
+    return 0;
+}
+
