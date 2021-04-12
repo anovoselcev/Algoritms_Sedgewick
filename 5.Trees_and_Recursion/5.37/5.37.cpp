b16a5f28e2de2e866e51d44dda847e25a246a784
smileamaxdev <novoseltsev.as@phystech.edu>
2021-04-12 21:22:21 +0300

    Fib(N) % M in dynamic programing style

---
+++ 5.Trees_and_Recursion/5.37/5.37.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+
+size_t Fib(size_t N) {
+    static size_t known[1000];
+    if (known[N]) return known[N];
+    size_t t = 1;
+    if (!N) return 0;
+    if (N > 1) t = Fib(N - 1) + Fib(N - 2);
+    return known[N] = t;
+}
+
+int Mod(size_t N, int M) {
+    return Fib(N) % M;
+}
+
+int main(){
+    size_t N;
+    int M;
+    std::cin >> N >> M;
+    std::cout << Fib(N) << std::endl;
+    std::cout << Mod(N, M) << std::endl;
+}
