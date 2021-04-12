54d193e1bafa89b0ff8f61e4d96b3e63e0acd761
smileamaxdev <novoseltsev.as@phystech.edu>
2021-04-12 21:55:57 +0300

    Recursion program P[N] = N / 2 + P[floor(N/2)] + P[ceil(N/2)] using dynamic programing style

---
+++ 5.Trees_and_Recursion/5.40/5.40.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+
+size_t Func(size_t N) {
+    static size_t* know = [] {
+        size_t* ptr = new size_t[10000];
+        std::memset(ptr, 0, 10000);
+        return ptr;
+    }();
+    if (N == 0) return know[0] = 0;
+    if (know[N]) return know[N];
+    return know[N] = N / 2 + Func(std::floor(N / 2)) + Func(std::ceil(N / 2));
+    
+}
+
+int main(){
+    size_t N;
+    std::cin >> N;
+    std::cout << Func(N) << std::endl;;
+}
