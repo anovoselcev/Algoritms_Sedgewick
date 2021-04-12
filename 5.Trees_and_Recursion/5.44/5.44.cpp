27374fd978fa713ce5dbb33bace22316012dd4ef
smileamaxdev <novoseltsev.as@phystech.edu>
2021-04-12 22:07:28 +0300

    Recursion program C[N] = N + 1/N + {i = 1:N -> C[i] + C[N - i]} using dynamic programing style

---
+++ 5.Trees_and_Recursion/5.44/5.44.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+
+
+double CN(size_t N) {
+    static double* know = [] {
+        double* ptr = new double[10000];
+        std::memset(ptr, 0, 10000);
+        return ptr;
+    }();
+
+    if (N == 0) return know[0] = 1;
+    if (know[N]) return know[N];
+    
+    for (size_t i = 1; i < N; ++i) know[N] += CN(i - 1) + CN(N - i);
+    return know[N] = N + know[N] / static_cast<double>(N);
+}
+
+int main(){
+    size_t N;
+    std::cin >> N;
+    std::cout << CN(N) << std::endl;
+}
