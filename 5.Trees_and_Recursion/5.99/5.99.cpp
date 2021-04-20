b59c5781cc3bf38c3bd007316559184f7eb96d56
smileamaxdev <novoseltsev.as@phystech.edu>
2021-04-20 21:17:40 +0300

    recursion Depth-first search

---
+++ 5.Trees_and_Recursion/5.99/5.99.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <unordered_map>
+
+template<typename T>
+struct Node {
+    T value;
+    Node* root = nullptr;
+    Node* next = nullptr;
+};
+
+struct check {
+    bool done = false;
+};
+
+
+template<typename T>
+Node<T>* traverse(Node<T>** graph, Node<T>* node) {
+    if (!graph || !node) return nullptr;
+
+    static std::unordered_map<Node<T>*, check> visited = { {nullptr, {true}} };
+    visited[node].done = true;
+    for (Node<T>* ptr = node; ptr; ptr = ptr->next)
+        if (!visited[ptr->root].done) traverse(graph, ptr->root);
+
+}
+
+
+int main() {
+    Node<double>** graph = new Node<double>*[3];
+    for (size_t i = 0; i < 3; ++i) graph[i] = new Node<double>[1];
+    graph[0][0] = { 0, graph[2] };
+    graph[1][0] = { 1, graph[0] };
+    graph[2][0] = { 2, graph[1] };
+    traverse(graph, graph[0]);
+}
